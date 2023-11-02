3110202d8de12fb0fb7602542efbeab34af00171
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2023-11-01 19:42:24 -0700

    fixed "infrantry" typo

--- lua/weapons/arc9_myt_ins1_m14.lua
+++ lua/weapons/arc9_myt_ins1_m14.lua
@@ -11,7 +11,7 @@ SWEP.PrintName = "M14"
 SWEP.Class = "Rifle"
 
 SWEP.Credits = {
-    ["Assets"] = [[Insurgency: Modern Infrantry Combat]],
+    ["Assets"] = [[Insurgency: Modern Infantry Combat]],
 }
 
 SWEP.ViewModel = "models/weapons/myt_ins1/c_sr_m14.mdl"
