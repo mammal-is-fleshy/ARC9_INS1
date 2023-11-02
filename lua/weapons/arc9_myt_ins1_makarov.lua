68899e41a4deedfe3643059fc07c8b65ca25e658
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2023-11-01 19:42:42 -0700

    fixed "infrantry" typo

--- lua/weapons/arc9_myt_ins1_makarov.lua
+++ lua/weapons/arc9_myt_ins1_makarov.lua
@@ -11,7 +11,7 @@ SWEP.PrintName = "Makarov"
 SWEP.Class = "Pistol"
 
 SWEP.Credits = {
-    ["Assets"] = [[Insurgency: Modern Infrantry Combat]],
+    ["Assets"] = [[Insurgency: Modern Infantry Combat]],
 }
 
 SWEP.ViewModel = "models/weapons/myt_ins1/c_pist_makarov.mdl"
